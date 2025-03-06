ac9f013cfede93bbffdd52c3afa06af70b28427a
AlexU9941 <awuchto@ilstu.edu>
2025-03-05 19:24:11 -0600

    Update DCL51.h

--- Rules/DCL51.h
+++ Rules/DCL51.h
@@ -1,3 +1,11 @@
+/**
+ * @file DCL51.h
+ * @brief Header file for the TestClass.
+ *
+ * This file contains the definition of the TestClass.
+ * It fulfills the DCL51-CPP rule by ensuring that all header files have include guards.
+ */
+
 #ifndef _DCL51_Header
 #define _DCL51_Header
 
