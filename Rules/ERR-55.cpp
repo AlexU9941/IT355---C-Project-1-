98f44d05be83b24e03de88b3a966d92f39dcb374
jvand01 <151086107+jvand01@users.noreply.github.com>
2025-03-05 21:11:31 -0600

    Update ERR-55.cpp

--- Rules/ERR-55.cpp
+++ Rules/ERR-55.cpp
@@ -1,7 +1,20 @@
+/**
+ * @file ERR-55.cpp
+ * @brief Demonstrates rule ERR-55 in C++.
+ * 
+ * This file contains a function that is explicitly marked as noexcept, 
+ * indicating that it does not throw exceptions.
+ */
+
 #include <iostream>
 #include <stdexcept>
 
-// Function marked noexcept does not throw exceptions
+/**
+ * @brief A function that does not throw exceptions.
+ * 
+ * This function is marked with noexcept, guaranteeing that it will not 
+ * propagate exceptions.
+ */
 void safeFunction() noexcept {
     std::cout << "safeFunction() executing safely.\n";
 }
