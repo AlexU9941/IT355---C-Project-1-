b06fb0d57764c10ec7364e1a808f51de36c91ed3
AlexU9941 <awuchto@ilstu.edu>
2025-03-05 19:35:31 -0600

    Update DCL54.cpp

--- Rules/DCL54.cpp
+++ Rules/DCL54.cpp
@@ -1,21 +1,59 @@
+/**
+ * @file example.cpp
+ * @brief Example file demonstrating custom new and delete operators.
+ *
+ * This file contains an example of how to implement custom new and delete operators
+ * for a class, fulfilling the DCL54-CPP rule by ensuring that all memory allocation
+ * and deallocation operations are properly handled.
+ */
+
 #include <iostream> 
 #include <new> 
+
+/**
+ * @brief Updates bookkeeping information for memory allocation and deallocation.
+ *
+ * This function is called whenever memory is allocated or deallocated to update
+ * the bookkeeping information variable seen in here.
+ *
+ * @param allocated_ptr Pointer to the allocated or deallocated memory.
+ * @param size Size of the allocated or deallocated memory.
+ * @param alloc True if memory is being allocated, false if it is being deallocated.
+ */
 
 extern "C++" void update_bookkeeping(void *allocated_ptr, std::size_t size, bool alloc);
 
 struct S {
+/**
+ *@struct S
+ *@brief A new structure with custom new and delete operators. 
+ *This unique structure demonstrates how to implement custom new and deletion operators
+ *to better update bookkeeping information
+ */
     void *operator new(std::size_t size) noexcept(false){
         void *ptr = ::operator new(size);
         update_bookkeeping(ptr, size, true);
         return ptr; 
     }
-
+/**
+ *@brief Custom-built delete operator.
+ *Correctly deallocates memory for any object of type S and updates bookkeeping information.
+ *@param ptr (Pointer) to the memory to be deallocated.
+ *@param size (Size) of the memory to be deallocated.
+ */
     void operator delete(void *ptr, std::size_t size) noexcept{
         ::operator delete(ptr);
         update_bookkeeping(ptr, size, false);
     }
 }; 
 
+/**
+ *@brief Updates the bookkeeping information.
+ *Correctly deallocates memory for any object of type S and updates bookkeeping information.
+ *@param allocated_ptr (Allocated pointer) to the allocated or deallocated memory.
+ *@param size (Size) of the allocated or deallocated memory.
+ *@param alloc (True) if the memory is being allocated, (False) if being deallocated.
+ */
 void update_bookkeeping(void *allocated_ptr, std::size_t size, bool alloc){
     std::cout<< "update_bookkeeping called with ptr: " << allocated_ptr << " , size: " << size << " , alloc: " << alloc << std::endl;  
 }
@@ -25,3 +63,4 @@ int main(){
     delete obj;
     return 0; 
 }
+
