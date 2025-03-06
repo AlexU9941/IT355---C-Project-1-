9d80207da5c682317f2a277e4db37ed807d1ebf7
jvand01 <151086107+jvand01@users.noreply.github.com>
2025-03-05 22:00:21 -0600

    Update ERR-59-2.cpp

--- Rules/ERR-59-2.cpp
+++ Rules/ERR-59-2.cpp
@@ -1,5 +1,22 @@
-#include "ERR-59-2.h"
+/**
+ * @file ERR-59-2.cpp
+ * @brief Demonstrates rule ERR-59 in C++. Library file.
+ * 
+ * This file defines the `riskyFunction` which simulates an error by setting 
+ * an error message and returning a failure code, without throwing an exception.
+ */
 
+#include "ERR-59-2.h" ///< Header file defining the ErrorCode enum and function signature
+
+/**
+ * @brief Simulates a failure and sets an error message.
+ * 
+ * This function sets the error message passed by reference and returns 
+ * an ErrorCode::FAILURE to indicate that the operation has failed.
+ * 
+ * @param errorMessage A reference to a string that will be set with the error message.
+ * @return ErrorCode::FAILURE to indicate failure.
+ */
 ErrorCode riskyFunction(std::string& errorMessage) {
     // Simulate an error without throwing an exception
     errorMessage = "Operation failed due to an internal issue.";
