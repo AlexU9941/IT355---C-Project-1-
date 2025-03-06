1788aa15765e781f0cff3085005bfe14f1af6082
AlexU9941 <awuchto@ilstu.edu>
2025-03-05 19:46:23 -0600

    Update DCL11.c

--- Recommendations/DCL11.c
+++ Recommendations/DCL11.c
@@ -1,6 +1,25 @@
+/**
+ * @file example.c
+ * @brief Example file demonstrating the use of variadic functions for error printing.
+ *
+ * This file contains an example of how to implement and use a variadic function
+ * to print error messages, fulfilling the DCL11-C rule by ensuring that all variadic
+ * functions are properly documented and used.
+ */
+
+
 #include <stdio.h>
 #include <stdarg.h>
 
+
+/**
+ * @brief Prints an error message.
+ *
+ * This function prints an error message using a variadic argument list.
+ *
+ * @param format The format string for the error message.
+ * @param ... Additional arguments for the format string.
+ */
 void print_error(const char* format, ...){
     va_list args; 
 
@@ -11,7 +30,14 @@ void print_error(const char* format, ...){
     va_end(args);
 }
 
-
+/**
+ * @brief Main function.
+ *
+ * This is the entry point of the program. It calls the print_error function with
+ * a format string and arguments, and prints the result to the console.
+ *
+ * @return 0 on success.
+ */
 int main(){
 const char *error_msg = "Error Occured"; 
     print_error("%d: %s\n", 15, error_msg); 
