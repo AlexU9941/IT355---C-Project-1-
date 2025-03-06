9720b869f299d9da7eb7fc976bb09b228d60367f
AlexU9941 <awuchto@ilstu.edu>
2025-03-05 19:28:48 -0600

    Update DCL51.cpp

--- Rules/DCL51.cpp
+++ Rules/DCL51.cpp
@@ -3,11 +3,23 @@
 #include <string> 
 using namespace std;
 
-
+/**
+ * @brief Implementation of the test method
+ * Method prints "This is my test." to the console and returns "True!" 
+ *
+ * @returns "True!" upon success.
+ */
 std::string TestClass::test() const{
     std::cout<<"This is my test."<< std::endl;
     return "True!"; 
 }
+
+/**
+ * @brief Main function definition here
+ * Entry point of the program here. Creats an instance of TestClass, before calling the 
+ *testing method, and prints the result to the console.
+ * @returns 0 upon success.
+ */
 
 int main() {
     TestClass dcl_obj;
