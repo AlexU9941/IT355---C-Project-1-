27d632009e7fb802d9416ba6c37b7a37a06d14e4
jvand01 <151086107+jvand01@users.noreply.github.com>
2025-03-05 21:09:35 -0600

    Update ERR-54.cpp

--- Rules/ERR-54.cpp
+++ Rules/ERR-54.cpp
@@ -1,22 +1,65 @@
+/**
+ * @file ERR-54.cpp
+ * @brief Demonstrates rule ERR-54 in C++.
+ * 
+ * This file defines a base exception class and a derived exception class, 
+ * both inheriting from std::exception. It also defines a class that throws 
+ * exceptions conditionally and showcases proper exception handling.
+ */
+
 #include <iostream>
 #include <exception>
 
+/**
+ * @class baseClass
+ * @brief A custom exception class derived from std::exception.
+ * 
+ * This class overrides the what() function to return a custom message.
+ */
 class baseClass : public std::exception {
 public:
+    /**
+     * @brief Returns an error message describing the exception.
+     * @return A C-string containing the exception message.
+     */
     virtual const char* what() const noexcept override {
         return "baseClass occurred";
     }
 };
 
+/**
+ * @class derivedClass
+ * @brief A derived exception class extending baseClass.
+ * 
+ * This class overrides the what() function to provide a more specific error message.
+ */
 class derivedClass : public baseClass {
 public:
+    /**
+     * @brief Returns an error message describing the derived exception.
+     * @return A C-string containing the derived exception message.
+     */
     virtual const char* what() const noexcept override {
         return "derivedClass occurred";
     }
 };
 
+/**
+ * @class MyClass
+ * @brief A class containing a function that may throw exceptions.
+ * 
+ * This class has a method that throws either a baseClass or derivedClass 
+ * exception based on the input parameter.
+ */
 class MyClass {
 public:
+    /**
+     * @brief Throws an exception based on the input flag.
+     * 
+     * @param throwDerived If true, throws a derivedClass exception. Otherwise, throws a baseClass exception.
+     * @throws derivedClass If throwDerived is true.
+     * @throws baseClass If throwDerived is false.
+     */
     void riskyFunction(bool throwDerived) {
         if (throwDerived) {
             throw derivedClass();
