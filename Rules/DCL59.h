f61aab01a577986e9f8235578061498abbba5d14
AlexU9941 <awuchto@ilstu.edu>
2025-03-05 19:43:33 -0600

    Update DCL59.h

--- Rules/DCL59.h
+++ Rules/DCL59.h
@@ -1,7 +1,15 @@
+/**
+ * @file DCL59.h
+ * @brief Header file for the delta namespace.
+ *
+ * This file contains the declaration of the external variable alpha within the delta namespace,
+ * fulfilling the DCL59-CPP rule by ensuring that all declarations are properly guarded.
+ */
+
 #ifndef DCL59_HEADER
 #define DCL59_HEADER
 
 namespace delta{
-    extern int alpha; 
+    extern int alpha; /**< External variable alpha declared in the delta namespace. */
 }
 #endif 
