7f9a8d647b84108ff9877459730bba8fc72f1c1d
AlexU9941 <awuchto@ilstu.edu>
2025-03-05 19:44:23 -0600

    Update DCL59.cpp

--- Rules/DCL59.cpp
+++ Rules/DCL59.cpp
@@ -1,7 +1,16 @@
 #include "DCL59.h"
 #include <iostream>
 
+/**
+ * @brief Definition of the external variable alpha.
+ */
 int alpha; 
+
+/**
+ * @brief Function that prints and modifies the value of alpha.
+ *
+ * This function prints the current value of alpha, then sets it to 36.
+ */
 
 void alphaFunction(){
     std::cout<< "Alpha Function(): "<< alpha <<std::endl;
@@ -9,13 +18,25 @@ void alphaFunction(){
 
 }
 
-
+/**
+ * @brief Function that prints and modifies the value of alpha.
+ *
+ * This function prints the current value of alpha, then sets it to 54.
+ */
 void betaFunction(){
     std::cout<< "Beta Function(): "<< alpha <<std::endl;
     alpha = 54; 
 
 }
 
+/**
+ * @brief Main function.
+ *
+ * This is the entry point of the program. It calls alphaFunction and betaFunction
+ * multiple times to demonstrate their behavior.
+ *
+ * @return 0 on success.
+ */
 int main(){
     extern void alphaFunction();
     alphaFunction();
