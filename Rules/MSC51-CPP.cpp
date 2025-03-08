6beaf3026387102f1a0d28bf6d5fc06d6513a540
BradyTOlson <159966964+BradyTOlson@users.noreply.github.com>
2025-03-07 18:35:39 -0600

    Create MSC51-CPP.cpp

---
+++ Rules/MSC51-CPP.cpp
@@ -0,0 +1,42 @@
+//Brady Olson
+//IT355
+//code example
+//MSC51-CPP
+
+#include <string>
+#include <random>
+#include <iostream>
+using namespace std;
+
+
+int randomNumberGenerator()
+{
+    uniform_int_distribution<int> distribution(1,10);
+
+    random_device rd;
+    mt19937 engine(rd());
+
+    return distribution(engine);
+}
+
+int main()
+{
+    int x, randNum;
+
+    randNum = randomNumberGenerator();
+
+cout<<"Guess a number 1-10"<<endl;
+cin>>x;
+
+if(x == randNum)
+{
+    cout<<"\nCongrats you Won!"<<endl;
+}else{
+    cout<<"\nYou Lost!"<<endl;
+}
+
+cout<<"The correct number was "<<randNum<<endl;
+
+
+}
+
