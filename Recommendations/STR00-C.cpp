f6d7f6942e5f8a0da502678d7075a6e54dd7c4d0
BradyTOlson <159966964+BradyTOlson@users.noreply.github.com>
2025-03-07 18:32:15 -0600

    Create STR00-C.cpp

---
+++ Recommendations/STR00-C.cpp
@@ -0,0 +1,18 @@
+//Brady Olson
+//IT355
+//Recommendation STR00-C
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    //correct variable typing
+    char ch = 'A'; 
+
+    cout << "Character: " << ch << endl; // Correctly prints 'A'
+
+    return 0;
+}
+
